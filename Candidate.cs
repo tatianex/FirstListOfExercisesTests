2bc05f1c70c6a38e58ef4f18737a04d291676a6a
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-02 19:36:29 -0300

    fixed security problems, added private to all properties in the class candidate

--- Candidate.cs
+++ Candidate.cs
@@ -5,9 +5,9 @@ namespace entra21_tests
     public class Candidate
     {
         public Guid Id{ get; private set; }
-        public string Name { get; set; }
-        public string Cpf { get; set; }
-        public int Votes { get; set; }
+        public string Name { get; private set; }
+        public string Cpf { get; private set; }
+        public int Votes { get; private set; }
 
         // CONSTRUTOR DA CLASSE CANDIDATE
         public Candidate(string name, string cpf)
