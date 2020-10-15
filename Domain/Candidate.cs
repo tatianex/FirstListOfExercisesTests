222e8a9072c2999139f98378563547f466fd471c
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-15 18:17:20 -0300

    added method to validate name and corrected bugs on CandidateTests

--- Domain/Candidate.cs
+++ Domain/Candidate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Domain
@@ -8,33 +9,21 @@ namespace Domain
         public Guid Id { get; private set; } = Guid.NewGuid();
         public string Name { get; private set; }
         public string CPF { get; private set; }
-        public int Votes { get; private set; }
+        public int Votes { get; private set; } = 0;
 
-        // CONSTRUTOR DA CLASSE CANDIDATE
-        public Candidate(string name, string cpf)
-        {
-            Name = name;
-            CPF = cpf;
-        }
-
-        public void Vote()
-        {
-            Votes++;
-        }
-
-        public bool Validate()
+        private bool ValidateCPF()
         {
             if (string.IsNullOrEmpty(CPF)) return false;
 
             var cpf = CPF.Replace(".", "").Replace("-", "");
             
-            if (cpf.Length != 11) return false;            
+            if (cpf.Length != 11) return false;
 
             if (!cpf.All(char.IsNumber)) return false;
-            
+
             var first = cpf[0];
             if (cpf.Substring(1, 10).All(x => x == first)) return false;
-            
+
             int[] multiplier1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplier2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
 
@@ -73,6 +62,49 @@ namespace Domain
             if (cpf.EndsWith(digit)) return true;
 
             return false;
+        }
+
+        private bool ValidateName()
+        {
+            if (string.IsNullOrEmpty(Name)) return false;
+
+            var words = Name.Split(' ');
+            if (words.Length < 2) return false;
+            
+
+            foreach (var word in words)
+            {
+                if (word.Trim().Length < 2) return false;
+                
+                if (word.Any(x => !char.IsLetter(x))) return false;
+            }
+
+            return true;
+        }
+
+        public Candidate(string name, string cpf)
+        {
+            Name = name;
+            CPF = cpf;
+        }
+
+        public void Vote()
+        {
+            Votes++;
+        }
+    
+        public (IList<string> errors, bool isValid) Validate()
+        {
+            var errors = new List<string>();
+            if (!ValidateCPF())
+            {
+                errors.Add("CPF inválido.");
+            }
+            if (!ValidateName())
+            {
+                errors.Add("Nome inválido.");
+            }
+            return (errors, errors.Count == 0);
         }
     }
 }
